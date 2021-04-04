960796b2d9707f889c13f458a0a9ad5e95cb4c46
Mark Pieciak <mpieciak18@msn.com>
2021-04-04 08:45:25 -0400

    add displayWin, displayTurn, & checkForWin functions and gameDone & statusMessage variables

--- scripts.js
+++ scripts.js
@@ -1,11 +1,83 @@
 const gameBoard = (function() {
-    const plays = [null, null, null, null, null, null, null, null, null, null];
+    const plays = [null, null, null, null, null, null, null, null, null];
+
+    // sets user to play first
+    let whoseTurn = 1;
+
+    let gameDone = false;
 
     const squares = document.getElementsByClassName('grid-square');
-    squaresArr = Array.from(squares);
+
+    const squaresArr = Array.from(squares);
+
+    const checkForWin = function() {
+        if (plays[0] != null && plays[0] == plays [1] && plays [0] == plays[2] ||
+            plays[3] != null && plays[3] == plays [4] && plays [3] == plays[5] ||
+            plays[6] != null && plays[6] == plays [7] && plays [6] == plays[9] ||
+            plays[0] != null && plays[0] == plays [3] && plays [0] == plays[6] ||
+            plays[1] != null && plays[1] == plays [4] && plays [1] == plays[7] ||
+            plays[2] != null && plays[2] == plays [5] && plays [2] == plays[8] ||
+            plays[0] != null && plays[0] == plays [4] && plays [0] == plays[8] ||
+            plays[2] != null && plays[2] == plays [4] && plays [2] == plays[6]) {
+            return true;
+        } else {
+            return false;
+        };
+    };
+
+    const statusMessage = document.getElementById('status');
+
+    const displayWin = function() {
+        if (whoseTurn == 1) {
+            statusMessage.innerText = 'Player X wins!!!'
+        } else {
+            statusMessage.innerText = 'Player O wins!!!'
+        };
+    };
+
+    const displayTurn = function() {
+        if (whoseTurn == 1) {
+            statusMessage.innerText = `It is Player X\'s turn.`
+        } else {
+            statusMessage.innerText = `It is Player O\'s turn.`
+        };
+    };
+
+    const updatePlays = function(play, rawIndex) {
+        const index = Number(rawIndex);
+        plays[index] = play;
+    };
+
+    const changeWhoseTurn = function() {
+        // if whoseTurn == 1 then user plays
+        // else computer plays
+        whoseTurn *= -1;
+    };
 
     const playMove = function(event) {
-        event.target.innerHTML = 'X';
+        if (gameDone == true) {
+            return null;
+        };
+
+        let play = '';
+        if (whoseTurn == 1) {
+            event.target.innerHTML = 'X';
+            play = 'X';
+        } else {
+            event.target.innerHTML = 'O';
+            play = 'O';
+        };
+        
+        const rawIndex = event.target.id.slice(7);
+        updatePlays(play, rawIndex);
+
+        if (checkForWin() == true) {
+            displayWin();
+            gameDone = true;
+        } else {
+            changeWhoseTurn();
+            displayTurn();
+        };
     };
 
     for (let i = 0; i < squaresArr.length; i++) {
@@ -13,5 +85,5 @@ const gameBoard = (function() {
         square.addEventListener('click', playMove);
     }
 
-    // return {squares, playMove};
+    return {squaresArr, plays};
 })();
